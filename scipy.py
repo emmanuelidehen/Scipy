4436f5e0cee5f7ce0efdf7dbb3e4714af54b77e6
Emmanuel idehen <23498282+emmanuelidehen@users.noreply.github.com>
2020-09-20 23:24:46 -0500

    Add files via upload

--- scipy.py
+++ scipy.py
@@ -8,12 +8,27 @@ Created on Sun Sep 20 22:33:05 2020
 
 import numpy as np 
 from scipy import sparse
+import pandas as pd 
 
 """ 
 converting a numpy array to csr matrix format 
 """
 x = np.eye(4)
 print(format(x))
-print("=================")
+print("======== CSR_Matrix format =============")
 y = sparse.csr_matrix(x)
 print(y)
+print("This next space addreses the pandans library ")
+#create a dictionary
+data = {'Name':["emmanuel","idehen","Osagumwenro"],
+        'Age':[20,30,40],
+        'Major':['Machine','Learning','101']
+        }
+#pass the dictionary to a dataframe 
+data_frame = pd.DataFrame(data)
+#display the dictionary in an excel format or database format 
+display(data_frame)
+#query the table
+print("===========  My Query  =============")
+
+display(data_frame[data_frame.Age < 40])
